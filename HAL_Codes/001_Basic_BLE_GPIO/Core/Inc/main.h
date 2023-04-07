6d8f70e07d19cdc1460a07498023639407e67114
Saaransh Panjrath <84224236+Saaraansh@users.noreply.github.com>
2023-04-07 17:10:10 +0530

    new changes

---
+++ HAL_Codes/001_Basic_BLE_GPIO/Core/Inc/main.h
@@ -0,0 +1,108 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2023 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32wbxx_hal.h"
+#include "app_conf.h"
+#include "app_entry.h"
+#include "app_common.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+void MX_GPIO_Init(void);
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define Button_2_Pin GPIO_PIN_2
+#define Button_2_GPIO_Port GPIOA
+#define Button_1_Pin GPIO_PIN_3
+#define Button_1_GPIO_Port GPIOA
+#define Led_Blue_2_Pin GPIO_PIN_4
+#define Led_Blue_2_GPIO_Port GPIOA
+#define Led_Red_1_Pin GPIO_PIN_5
+#define Led_Red_1_GPIO_Port GPIOA
+#define Led_Red_2_Pin GPIO_PIN_6
+#define Led_Red_2_GPIO_Port GPIOA
+#define Led_Blue_1_Pin GPIO_PIN_7
+#define Led_Blue_1_GPIO_Port GPIOA
+#define Led_Green_1_Pin GPIO_PIN_9
+#define Led_Green_1_GPIO_Port GPIOA
+#define B1_Pin GPIO_PIN_4
+#define B1_GPIO_Port GPIOC
+#define LD2_Pin GPIO_PIN_0
+#define LD2_GPIO_Port GPIOB
+#define LD3_Pin GPIO_PIN_1
+#define LD3_GPIO_Port GPIOB
+#define Button_3_Pin GPIO_PIN_6
+#define Button_3_GPIO_Port GPIOC
+#define JTMS_Pin GPIO_PIN_13
+#define JTMS_GPIO_Port GPIOA
+#define JTCK_Pin GPIO_PIN_14
+#define JTCK_GPIO_Port GPIOA
+#define Led_Green_2_Pin GPIO_PIN_12
+#define Led_Green_2_GPIO_Port GPIOC
+#define B2_Pin GPIO_PIN_0
+#define B2_GPIO_Port GPIOD
+#define B3_Pin GPIO_PIN_1
+#define B3_GPIO_Port GPIOD
+#define JTDO_Pin GPIO_PIN_3
+#define JTDO_GPIO_Port GPIOB
+#define LD1_Pin GPIO_PIN_5
+#define LD1_GPIO_Port GPIOB
+void   MX_USART1_UART_Init(void);
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
